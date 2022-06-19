292525bc9630e7f967fcc1a913e5271b29d10e31
Joshua Morony <joshua.morony@gmail.com>
2022-06-20 09:23:48 +0930

    test for result type

--- src/app/home/data-access/photo/photo.service.spec.ts
+++ src/app/home/data-access/photo/photo.service.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { Camera, CameraResultType } from '@capacitor/camera';
 import { subscribeSpyTo } from '@hirez_io/observer-spy';
+import { Platform } from '@ionic/angular';
 import { PhotoService } from './photo.service';
 
 jest.mock('@capacitor/camera', () => ({
@@ -16,10 +18,16 @@ jest.mock('@capacitor/camera', () => ({
 
 describe('PhotoService', () => {
   let service: PhotoService;
+  let platform: Platform;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [Platform],
+    });
     service = TestBed.inject(PhotoService);
+    platform = TestBed.inject(Platform);
+
+    jest.clearAllMocks();
   });
 
   it('should be created', () => {
@@ -33,7 +41,15 @@ describe('PhotoService', () => {
       expect(observerSpy.getLastValue()?.[0].path).toContain('test-path');
     });
 
-    it('should use URI result type if running natively', () => {});
+    it('should use URI result type if running natively', async () => {
+      jest.spyOn(platform, 'is').mockReturnValue(true);
+
+      await service.takePhoto();
+
+      expect(Camera.getPhoto).toHaveBeenCalledWith(
+        expect.objectContaining({ resultType: CameraResultType.Uri })
+      );
+    });
 
     it('should use data url result type if running as PWA', () => {});
 
