c24add39afde4edf7200f49ba3f4c089dab39d41
Joshua Morony <joshua.morony@gmail.com>
2022-06-19 21:35:16 +0930

    implementation for takePhoto

--- src/app/home/ui/photo-list/photo-list.component.ts
+++ src/app/home/ui/photo-list/photo-list.component.ts
@@ -2,13 +2,26 @@ import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { PhotoService } from '../../data-access/photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
-  template: ``,
+  template: `
+    <ion-list>
+      <ion-item
+        data-test="take-photo-button"
+        button
+        (click)="photoService.takePhoto()"
+      >
+        take photo
+      </ion-item>
+    </ion-list>
+  `,
   styles: [],
 })
-export class PhotoListComponent {}
+export class PhotoListComponent {
+  constructor(protected photoService: PhotoService) {}
+}
 
 @NgModule({
   imports: [
