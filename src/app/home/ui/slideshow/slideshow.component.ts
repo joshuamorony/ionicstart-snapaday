10acf5f087ac6d616469599f8c11ce4f6888f1be
Joshua Morony <joshua.morony@gmail.com>
2022-06-24 11:44:38 +0930

    unit tests for slideshow

--- src/app/home/ui/slideshow/slideshow.component.ts
+++ src/app/home/ui/slideshow/slideshow.component.ts
@@ -1,9 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-slideshow',
-  template: ``,
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title></ion-title>
+        <ion-buttons>
+          <ion-button data-test="play-button">
+            <ion-icon name="play" slot="icon-only"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <img data-test="slideshow-image" src="" />
+    </ion-content>
+  `,
   styles: [``],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SlideshowComponent {}
+
+@NgModule({
+  declarations: [SlideshowComponent],
+  imports: [IonicModule, CommonModule],
+  exports: [SlideshowComponent],
+})
+export class SlideshowComponentModule {}
