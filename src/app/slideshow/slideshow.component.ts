8597dc15c741137d8ec445cba507f8c4e9a56941
Joshua Morony <joshua.morony@gmail.com>
2022-10-27 16:05:31 +1030

    add extra features

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -6,14 +6,30 @@ import {
   NgModule,
 } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
-import { BehaviorSubject, from, of, timer } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  EMPTY,
+  from,
+  iif,
+  NEVER,
+  of,
+  race,
+  Subject,
+  timer,
+} from 'rxjs';
 import {
   concatMap,
   delay,
   delayWhen,
+  expand,
+  finalize,
+  repeat,
+  repeatWhen,
   startWith,
   switchMap,
   tap,
+  withLatestFrom,
 } from 'rxjs/operators';
 import { Photo } from '../shared/interfaces/photo';
 import { SlideshowImageComponentModule } from './ui/slideshow-image.component';
@@ -40,12 +56,15 @@ import { SlideshowImageComponentModule } from './ui/slideshow-image.component';
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <app-slideshow-image
-        (mousedown)="paused$.next(true)"
-        (mouseup)="paused$.next(false)"
-        *ngIf="currentPhoto$ | async as photo"
-        [safeResourceUrl]="photo.safeResourceUrl"
-      ></app-slideshow-image>
+      <ng-container *ngIf="currentPhoto$ | async as photo">
+        <app-slideshow-image
+          (mousedown)="paused$.next(true)"
+          (mouseup)="paused$.next(false); manualPhoto$.next(null)"
+          [safeResourceUrl]="photo.safeResourceUrl"
+        ></app-slideshow-image>
+        <ion-button (click)="prevPhoto(photo)">Prev</ion-button>
+        <ion-button (click)="nextPhoto(photo)">Next</ion-button>
+      </ng-container>
     </ion-content>
   `,
   styles: [
@@ -58,21 +77,73 @@ import { SlideshowImageComponentModule } from './ui/slideshow-image.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SlideshowComponent {
-  paused$ = new BehaviorSubject(false);
-  delay$ = this.paused$.pipe(
-    switchMap((isPaused) => timer(isPaused ? 100000 : 1000))
+  paused$ = new BehaviorSubject(false); // pause/play slideshow
+  delayTime$ = new BehaviorSubject(1000); // time between photos
+  loop$ = new BehaviorSubject(true); // loop slideshow
+  manualPhoto$ = new BehaviorSubject<Photo | null>(null); // set photo manually
+
+  delay$ = combineLatest([this.paused$, this.delayTime$]).pipe(
+    switchMap(([isPaused, delayTime]) =>
+      iif(() => isPaused, NEVER, timer(delayTime))
+    )
   );
 
   currentPhotos$ = new BehaviorSubject<Photo[]>([]);
-  currentPhoto$ = this.currentPhotos$.pipe(
-    switchMap((photos) => from(photos)),
-    concatMap((photo) => of(photo).pipe(delayWhen(() => this.delay$)))
+
+  currentPhoto$ = combineLatest([this.currentPhotos$, this.manualPhoto$]).pipe(
+    switchMap(([_, manualPhoto]) =>
+      iif(
+        // If manual photo set, use that instead
+        () => manualPhoto !== null,
+        of(manualPhoto),
+        this.playCurrentPhotos().pipe(
+          // Play recursively if loop is set to true
+          expand((photo) => {
+            const currentPhotos = this.currentPhotos$.value;
+            const isLastPhoto =
+              photo === currentPhotos[currentPhotos.length - 1];
+            return isLastPhoto && this.loop$.value
+              ? this.playCurrentPhotos()
+              : EMPTY;
+          })
+        )
+      )
+    )
   );
 
   constructor(protected modalCtrl: ModalController) {}
 
   @Input() set photos(value: Photo[]) {
     this.currentPhotos$.next([...value].reverse());
+  }
+
+  playCurrentPhotos() {
+    return this.currentPhotos$.pipe(
+      switchMap((photos) => from(photos)),
+      concatMap((photo) => of(photo).pipe(delayWhen(() => this.delay$)))
+    );
+  }
+
+  nextPhoto(currentPhoto: Photo) {
+    this.paused$.next(true);
+
+    const currentPhotos = this.currentPhotos$.value;
+    const currentIndex = currentPhotos.indexOf(currentPhoto);
+    const nextIndex =
+      currentIndex < currentPhotos.length - 1 ? currentIndex + 1 : 0;
+
+    this.manualPhoto$.next(currentPhotos[nextIndex]);
+  }
+
+  prevPhoto(currentPhoto: Photo) {
+    this.paused$.next(true);
+
+    const currentPhotos = this.currentPhotos$.value;
+    const currentIndex = currentPhotos.indexOf(currentPhoto);
+    const prevIndex =
+      currentIndex > 0 ? currentIndex - 1 : currentPhotos.length - 1;
+
+    this.manualPhoto$.next(currentPhotos[prevIndex]);
   }
 }
 
