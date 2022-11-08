7576d0390c0e9d20fe6cdbba1697ffd4be1850d1
Joshua Morony <joshua.morony@gmail.com>
2022-11-08 10:37:14 +1030

    fix: add tests for photo stream

--- src/app/slideshow/slideshow.component.spec.ts
+++ src/app/slideshow/slideshow.component.spec.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy } from '@angular/core';
 import {
   ComponentFixture,
+  discardPeriodicTasks,
   fakeAsync,
   TestBed,
   tick,
   waitForAsync,
 } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { subscribeSpyTo } from '@hirez_io/observer-spy';
 import { IonicModule } from '@ionic/angular';
 import { Photo } from '../shared/interfaces/photo';
@@ -45,30 +47,139 @@ describe('SlideshowComponent', () => {
   });
 
   describe('@Input() photos', () => {
-    it('when it is launched, it should show every photo in sequence', fakeAsync(() => {
-      const observerSpy = subscribeSpyTo(component.currentPhoto$);
-      component.photos = testPhotos;
+    const getSlideshowImage = () =>
+      fixture.debugElement.query(By.css('app-slideshow-image'));
+
+    const getResourceUrl = () =>
+      getSlideshowImage().componentInstance.safeResourceUrl;
+
+    const waitForDelay = () => {
+      tick(1000);
       fixture.detectChanges();
+    };
 
-      expect(observerSpy.getLastValue()).toBe(undefined);
+    beforeEach(() => {
+      component.loop$.next(false);
+      component.delayTime$.next(1000);
+      component.staticPhoto$.next(null);
+      component.paused$.next(false);
+    });
 
-      tick(500);
+    it('should set safeResourceUrl of app-slideshow-image to each of the photos in the array with delay', fakeAsync(() => {
+      component.photos = testPhotos;
 
-      expect(observerSpy.getLastValue()?.safeResourceUrl).toEqual(
-        testPhotos[testPhotos.length - 1].safeResourceUrl
-      );
-
-      tick(500);
-
-      expect(observerSpy.getLastValue()?.safeResourceUrl).toEqual(
-        testPhotos[testPhotos.length - 2].safeResourceUrl
-      );
-
-      tick(500);
-
-      expect(observerSpy.getLastValue()?.safeResourceUrl).toEqual(
-        testPhotos[testPhotos.length - 3].safeResourceUrl
-      );
+      for (let i = testPhotos.length - 1; i >= 0; i--) {
+        waitForDelay();
+        expect(getResourceUrl()).toBe(testPhotos[i].safeResourceUrl);
+      }
     }));
+
+    describe('paused$', () => {
+      it('should go to next photo if false', fakeAsync(() => {
+        component.paused$.next(false);
+        component.photos = testPhotos;
+        fixture.detectChanges();
+
+        waitForDelay();
+        expect(getResourceUrl()).toBe(
+          testPhotos[testPhotos.length - 1].safeResourceUrl
+        );
+        waitForDelay();
+        expect(getResourceUrl()).toBe(
+          testPhotos[testPhotos.length - 2].safeResourceUrl
+        );
+
+        discardPeriodicTasks();
+      }));
+
+      it('should not go to next photo if true', fakeAsync(() => {
+        component.photos = testPhotos;
+        fixture.detectChanges();
+
+        waitForDelay();
+        component.paused$.next(true);
+
+        expect(getResourceUrl()).toBe(
+          testPhotos[testPhotos.length - 1].safeResourceUrl
+        );
+        waitForDelay();
+        expect(getResourceUrl()).toBe(
+          testPhotos[testPhotos.length - 1].safeResourceUrl
+        );
+
+        discardPeriodicTasks();
+      }));
+    });
+
+    describe('loop$', () => {
+      it('should loop if true', fakeAsync(() => {
+        component.loop$.next(true);
+        component.photos = testPhotos;
+        fixture.detectChanges();
+
+        testPhotos.forEach((photo) => waitForDelay());
+
+        waitForDelay();
+
+        expect(getResourceUrl()).toBe(
+          testPhotos[testPhotos.length - 1].safeResourceUrl
+        );
+
+        discardPeriodicTasks();
+      }));
+
+      it('should stop after all photos have been emitted if false', fakeAsync(() => {
+        component.loop$.next(false);
+        component.photos = testPhotos;
+        fixture.detectChanges();
+
+        testPhotos.forEach((photo) => waitForDelay());
+
+        expect(getResourceUrl()).toBe(testPhotos[0].safeResourceUrl);
+      }));
+    });
+
+    describe('staticPhoto$', () => {
+      it('should display static photo if static photo is set', fakeAsync(() => {
+        component.staticPhoto$.next(testPhotos[0]);
+        component.photos = testPhotos;
+        fixture.detectChanges();
+
+        waitForDelay();
+        waitForDelay();
+
+        fixture.detectChanges();
+
+        expect(getResourceUrl()).toBe(testPhotos[0].safeResourceUrl);
+      }));
+    });
+
+    describe('delayTime$', () => {
+      it('should change delay to specified amount', fakeAsync(() => {
+        component.photos = testPhotos;
+        fixture.detectChanges();
+
+        waitForDelay();
+        component.delayTime$.next(2000);
+
+        expect(getResourceUrl()).toBe(
+          testPhotos[testPhotos.length - 1].safeResourceUrl
+        );
+
+        waitForDelay();
+
+        expect(getResourceUrl()).toBe(
+          testPhotos[testPhotos.length - 1].safeResourceUrl
+        );
+
+        waitForDelay();
+
+        expect(getResourceUrl()).toBe(
+          testPhotos[testPhotos.length - 2].safeResourceUrl
+        );
+
+        discardPeriodicTasks();
+      }));
+    });
   });
 });
